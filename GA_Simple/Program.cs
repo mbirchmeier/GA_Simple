1b28d1f63c0fa5c387865be3d59c2c80228b9b4c
MBirchmeier <github@mbirchmeier.com>
2012-09-09 10:50:11 -0400

    Setup main function
    Setup the main function and testing my first commit

---
+++ GA_Simple/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GA_Simple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int target = 0;
+            bool targetSet = false;
+            if (args.Count() == 1)
+            {
+                if (int.TryParse(args[1], out target))
+                {
+                    targetSet = true;
+                }
+            }
+            while (!targetSet)
+            {
+                Console.WriteLine("Enter target:");
+                string input = Console.ReadLine();
+                targetSet = int.TryParse(input, out target);
+            }
+
+            Console.WriteLine("Algorithm will target a value of {0}", target);
+
+            Console.WriteLine("Execution complete: Press any key to continue");
+            Console.ReadKey();
+        }
+    }
+}
